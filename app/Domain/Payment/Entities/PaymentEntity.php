244fffbf9331f519a0961b698e92e9067365fb77
Md Tarikul Islam <rony.ksr06@gmail.com>
2025-12-31 16:09:04 +0600

    Add Business Domain Feature structure

---
+++ app/Domain/Payment/Entities/PaymentEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Payment\Entities;
+
+class PaymentEntity
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Domain/Payment/ValueObjects/Money.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Payment\ValueObjects;
+
+class Money
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
