bfbb496c7c21203b08009a20cd1e0fcdd7f6b0cb
Md Tarikul Islam <rony.ksr06@gmail.com>
2026-01-03 13:38:59 +0600

    subscription domain and plan domain added

--- app/Domain/Subscription/Entities/PlanEntity.php
+++ app/Domain/Subscription/Entities/PlanEntity.php
@@ -7,8 +7,13 @@ class PlanEntity
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public readonly int $id,
+        public readonly string $name,
+        public readonly string $slug,
+        public readonly int $price,
+        public readonly string $currency,
+        public readonly bool $isActive,
+    )
+    {}
 }
--- app/Domain/Subscription/Entities/SubscriptionEntity.php
+++ app/Domain/Subscription/Entities/SubscriptionEntity.php
@@ -7,8 +7,11 @@ class SubscriptionEntity
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public readonly int $id,
+        public readonly int $userId,
+        public readonly int $planId,
+        public readonly string $status,
+    )
+    {}
 }
