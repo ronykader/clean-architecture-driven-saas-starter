96f88acdb363ac0c88d88b20fed6b1e115bb23d4
Md Tarikul Islam <rony.ksr06@gmail.com>
2026-01-13 18:04:19 +0600

    add StripWebhook verifyer Feature test

---
+++ tests/Feature/StripeWebhookVerifierTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Infrastructure\Payment\Stripe\StripeWebhookVerifier;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('accepts valid webhook signatures', function () {
+    $this->mock(StripeWebhookVerifier::class, function ($mock) {
+        $mock->shouldReceive('verify')->andReturn((object) [
+            'id' => 'evt_1',
+            'type' => 'checkout.session.completed',
+            'data' => (object) [
+                'object' => (object) ['id' => 'sess_123']
+            ]
+        ]);
+    });
+
+    $this->post('/webhooks/stripe', [], [
+        'Stripe-Signature' => 'valid'
+    ])->assertOk();
+});
